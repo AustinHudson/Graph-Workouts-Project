61d42152dc9620e0f22e385e422f856c3bf20e32
AustinHudson <hudsaust@oregonstate.edu>
2018-08-09 21:45:51 -0500

    Added workout models
    Added some more workout models and migrated the changes to the DB

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Exercises, People, Routines, ExerciseInRoutine
 # Register your models here.
+
+admin.site.register(Exercises)
+
+admin.site.register(Routines)
+
+admin.site.register(People)
+
+admin.site.register(ExerciseInRoutine)
--- workouts/models.py
+++ workouts/models.py
@@ -1,24 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 
-# Create your models here.
 
-class Routines(models.Model):
-    routine_name = models.CharField(max_length=100)
-    routine_description = models.TextField(max_length=200)
 
 class Exercises(models.Model):
     exercise_name = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
 
-#
-# Change this to many to many field in other classes
-#
-# class Routine_Exercises(models.Model):
-#
-#     default_sets = models.IntegerField
-#     default_reps = models.IntegerField
-#
-#
-#
+class Routines(models.Model):
+    routine_name = models.CharField(max_length=100)
+    routine_description = models.TextField(max_length=200)
+    exercises = models.ManyToManyField(Exercises)
+
+class People(User):
+    routines = models.ManyToManyField(Routines)
+
+class ExerciseInRoutine(models.Model):
+    exercise = models.ForeignKey(Exercises, on_delete=models.CASCADE)
+    routine = models.ForeignKey(Routines, on_delete=models.CASCADE)
+    default_sets = models.IntegerField()
+    default_reps = models.IntegerField()
+
+
